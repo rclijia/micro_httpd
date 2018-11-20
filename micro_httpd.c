233009e9f44ac674d27a51b8f58c7d240e24e52b
rclijia <chinalijia@126.com>
2018-11-20 11:55:48 +0800

    update to v0.2.3

--- micro_httpd.c
+++ micro_httpd.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <pthread.h>
@@ -13,35 +10,31 @@
 #include <netinet/in.h>
 #include <signal.h>
 
-/*
-	A simple HTTP server implemented in C language,  
-	used to help beginners understand socket, multithreading and other related knowledge.
-*/
+#define MHTTP_VERSION "v0.2.3"
+#define MHTTP_DATE    "20181120"
 #define DEBUG 1
 #if DEBUG
 #define DPRINT(fmt, args...) fprintf(stdout, fmt, ##args)
 #else
 #define DPRINT(fmt, args...)
 #endif
-
-#define THREAD_NUM			4  /* can be adjusted according to the hardware  */
-#define SERVER_LISTEN_PORT  80
-#define MAX_BUF_LEN			(2048)
+#define THREAD_NUM		4  /* can be adjusted according to the hardware  */
+#define SERVER_LISTEN_PORT  	80
+#define MAX_BUF_LEN		(2048)
 #define HTTPD_SERVER_IP		"127.0.0.1"
 
-typedef unsigned int			MHD_ATOMIC_T;
+typedef unsigned int		MHD_ATOMIC_T;
 #define MHD_ATOMIC_SET(v, i)	__sync_lock_test_and_set((&v), i)
-#define MHD_ATOMIC_READ(v)		__sync_or_and_fetch((&v), 0)
-#define MHD_ATOMIC_INC(v) 		__sync_add_and_fetch((&v),1)
-#define MHD_ATOMIC_DEC(v) 		__sync_sub_and_fetch((&v),1)
+#define MHD_ATOMIC_READ(v)	__sync_or_and_fetch((&v), 0)
+#define MHD_ATOMIC_INC(v) 	__sync_add_and_fetch((&v),1)
+#define MHD_ATOMIC_DEC(v) 	__sync_sub_and_fetch((&v),1)
 #define MHD_ATOMIC_ADD(v,add) 	__sync_add_and_fetch((&v),(add))
 #define MHD_ATOMIC_SUB(v,sub) 	__sync_sub_and_fetch((&v),(sub))
 
-#define HTTP_MAINPAGE_RES_HDR "HTTP/1.1 200 OK\r\nServer: MicroHttpServer-0.2.3\r\nContent-Type: %s\r\nContent-Length: %d\r\nConnection: close\r\n\r\n"
+#define HTTP_MAINPAGE_RES_HDR "HTTP/1.1 200 OK\r\nServer: MicroHttpServer-%s\r\nContent-Type: %s\r\nContent-Length: %d\r\nConnection: close\r\n\r\n"
 #define HTTP_MAINPAGE_CONT "<html><head><title>Hello, Micro Http!</title></head><body><h1>Hello, Micro Http!</h1></body></html>"
 
 static int httpd = -1;
-
 static pthread_t g_thread_pool[THREAD_NUM];
 static MHD_ATOMIC_T thread_in_use[THREAD_NUM];
 static volatile int thread_client_fd[THREAD_NUM];
@@ -58,7 +51,6 @@ static int mhd_start(void)
     struct sockaddr_in name;
 
     signal(SIGPIPE, SIG_IGN);
-
     httpd = socket(PF_INET, SOCK_STREAM, 0);
     if (httpd == -1){
         perror("socket");
@@ -77,90 +69,71 @@ static int mhd_start(void)
         perror("bind");
         return -1;
     }
-
     if (listen(httpd, THREAD_NUM) < 0){
         perror("listen");
         return -1;
-     }
-        
+    }
     return 0;
 }
-
 static inline int mhd_request_check(const char *data, int data_len)
 {
 	if(data_len < strlen("GET / HTTP/1.1\r\n\r\n")){ /* Less than minimum HTTP legal request length */
 		return -1;
 	}
-
 	if(memmem(data, data_len, "\r\n\r\n", 4) == NULL){ /* no http end flag */
 		return -1;
 	}
-
 	return 0;
 }
-
 static int mdh_process_request(int client_fd, char *rcv_buf, int buf_len)
 {
 	int rcv_len;
-	
 	rcv_len = read(client_fd, rcv_buf, buf_len);
 	if(rcv_len < 0){
 		DPRINT("client_fd:%d recv error, ret = %d, exit!\n", client_fd, rcv_len);
 		return -1;	
 	}
-
 	if(mhd_request_check(rcv_buf, rcv_len) < 0){
 		return -1;
 	}
-
 	return rcv_len;
 }
-
 static int mdh_send_response(int client_fd)
 {
 	char http_res_header[MAX_BUF_LEN];
 	int ret;
-
-	snprintf(http_res_header, MAX_BUF_LEN, HTTP_MAINPAGE_RES_HDR, "text/html", (int)strlen(HTTP_MAINPAGE_CONT));
-
+	snprintf(http_res_header, MAX_BUF_LEN, HTTP_MAINPAGE_RES_HDR, MHTTP_VERSION, "text/html", (int)strlen(HTTP_MAINPAGE_CONT));
 	/* send http response header */
 	ret = write(client_fd, http_res_header, strlen(http_res_header));
 	if(ret != strlen(http_res_header)){
 		DPRINT("sock write error, ret=%d!\n", ret);
 		return -1;
 	}
-
 	/* send http response content */
 	ret = write(client_fd, HTTP_MAINPAGE_CONT, strlen(HTTP_MAINPAGE_CONT));
 	if(ret != strlen(HTTP_MAINPAGE_CONT)){
 		DPRINT("sock write error, ret=%d!\n", ret);
 		return -1;
 	}
-
 	return 0;
 }
-
 static void mhd_do_work(int client_fd)
 {
 	char rcv_buf[MAX_BUF_LEN];
 	int rcv_len;
-	
 	rcv_len = mdh_process_request(client_fd, rcv_buf, MAX_BUF_LEN);
 	if(rcv_len < 0){
 		goto done;
 	}
-
 	mdh_send_response(client_fd);
 
 done:
 	close(client_fd);
 	return;
 }
-
 static void *mhd_work_thread(void *arg)
 {
 	int tid = *((int *)arg);
-	
 	while(1){
 		if(TSTATE_TO_WORK != MHD_ATOMIC_READ(thread_in_use[tid])){
 			usleep(1000);
@@ -168,19 +141,13 @@ static void *mhd_work_thread(void *arg)
 		}
 
 		DPRINT("thread %d ready to work, client fd is:%d\n", tid, thread_client_fd[tid]);
-
 		mhd_do_work(thread_client_fd[tid]);
-
 		thread_client_fd[tid] = -1;
-
 		DPRINT("thread %d word is done, wait for new work......\n", tid);
-
 		MHD_ATOMIC_SET(thread_in_use[tid], TSTATE_IDLE);
 	}
 	return NULL;
-
 }
-
 static int mhd_thread_pool_init(void)
 {
 	int i;
@@ -189,15 +156,11 @@ static int mhd_thread_pool_init(void)
 		__tid[i] = i;
 		pthread_create(&g_thread_pool[i], NULL, mhd_work_thread, &__tid[i]);
 	}
-
 	return 0;
 }
-
 static void mhd_search_idle_thread(int client_fd)
 {
-	int i;
-	int done = 0;
-
+	int i, done = 0;
 	while(0 == done){
 		for(i = 0; i < THREAD_NUM; i++){
 			if(TSTATE_IDLE == MHD_ATOMIC_READ(thread_in_use[i])){
@@ -211,35 +174,25 @@ static void mhd_search_idle_thread(int client_fd)
 		}
 		usleep(1000); /* no idle thread, wait a moment..... */
 	}
-
 	return;
 }
-
 int main(void)
 {
-	int ret;
-	int client_fd = -1;
+	int ret, client_fd = -1;
 	struct sockaddr_in client_addr;
 	socklen_t client_addr_len;
-
 	srand(time(NULL));
-
 	ret = mhd_start();
 	if(ret < 0){
 		exit(1);
 	}
-
 	mhd_thread_pool_init();
-	
 	DPRINT("mhttpd starting....\n");
-
 	while(1){
 		client_fd = accept(httpd, (struct sockaddr *)&client_addr, &client_addr_len);
 		if(client_fd > 0){
 			mhd_search_idle_thread(client_fd);
 		}
 	}
-	
 	return 0;
 }
-
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
 /*
-	A simple HTTP server implemented in C language,  
+	A simple HTTP server implemented in C language, just only 200 lines,
 	used to help beginners understand socket, multithreading and other related knowledge.
 */
